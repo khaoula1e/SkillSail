39d3a90eea3a08862c0e1c3a5a3d92c173ec559f
KhaoulaElfatimi <elfatimi.khaoula@inemail.ine.inpt.ma>
2023-10-04 02:32:06 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/images/Logo2.png';
+import { AiOutlineHistory, AiOutlineUser, AiOutlineArrowDown } from 'react-icons/ai';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import {FaHistory} from 'react-icons/fa';
+import {MdNotificationsActive} from 'react-icons/md';
+const Navbar = () => {
+  const menuItems = [
+    { icon: <FaHistory />, title: 'History' },
+    { icon: <MdNotificationsActive />, title: 'Notification' },
+    { icon: <AiOutlineUser />, title: 'Me', hasDropdown: true }, 
+  ];
 
-const Navbar =() => {
-    return (
-        <div>
-            <div></div>
-            <div>
-                <ul>
-                <li className='p-4'>History</li>
-                <li className='p-4'>Notification</li>
-                <li className='p-4'>Me</li>
-                </ul>
-            </div>
+  return (
+    <div className='bg-[#04003F] text-[#D9D9D9] py-4'>
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={logo} alt="SkillSail Logo" className="h-12 w-auto mr-2" /> 
         </div>
-    )
+        <ul className="flex space-x-4 font-semibold text-base">
+          {menuItems.map((item, index) => (
+            <li key={index} className="p-4 flex items-center">
+              {item.icon}
+              <span className="ml-2">{item.title}</span>
+              {item.hasDropdown && <IoMdArrowDropdown  className="ml-1" />} 
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
+
 export default Navbar;
